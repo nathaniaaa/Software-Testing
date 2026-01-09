f30f7a65f89cf1e3653f8369dde7ccf65c41bb17
nthnniaa <nataniaratnadewi@gmail.com>
2026-01-09 15:57:03 +0700

    test leaderboard

---
+++ src/test/java/tests/flow/challenge/TestLeaderboard.java
@@ -0,0 +1,68 @@
+package tests.flow.challenge;
+
+import tests.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.appium.java_client.AppiumBy;
+
+public class TestLeaderboard extends BaseTest {
+
+    // Daftar Lokasi
+    
+    // Ikon Challenge (Bottom Navigation)
+    By navChallenge = AppiumBy.xpath("//android.widget.Button[@text='Challenge Challenge']");
+
+    // Tab Leaderboard (Header Atas)
+    By tabLeaderboard = AppiumBy.xpath("//android.view.View[@content-desc='Leaderboard Leaderboard']");
+
+    // Validasi (Global Leaderboard)
+    By textGlobal = AppiumBy.xpath("//android.view.View[contains(@content-desc, 'Global') or contains(@text, 'Global')] | //android.widget.TextView[contains(@text, 'Global')]");
+
+
+    // Test Cases
+    @Test(priority = 1)
+    public void testMasukMenuChallenge() {
+        System.out.println("TEST 1: Buka Tab Challenge");
+        
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        
+        // Klik Challenge di Bottom Navigation
+        click(navChallenge);
+        
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        
+        // Validasi: pastikan tombol Leaderboard di atas muncul
+        waitForVisibility(tabLeaderboard);
+        Assert.assertTrue(driver.findElements(tabLeaderboard).size() > 0, "Menu Challenge gagal terbuka!");
+        
+        takeScreenshot("Halaman_Challenge_Utama");
+    }
+
+    @Test(priority = 2)
+    public void testMasukLeaderboard() {
+        System.out.println("TEST 2: Klik Ikon Leaderboard");
+
+        // Klik ikon piala (Leaderboard)
+        click(tabLeaderboard);
+        
+        // Jeda loading data leaderboard 
+        System.out.println("Menunggu data leaderboard");
+        try { Thread.sleep(4000); } catch (Exception e) {} 
+        
+        takeScreenshot("Halaman_Leaderboard_Terbuka");
+    }
+
+    @Test(priority = 3)
+    public void testScrollLeaderboard() {
+        System.out.println("TEST 3: Scroll Daftar Leaderboard");
+
+        // Scroll ke bawah pelan-pelan (Swipe 1)
+        System.out.println("Scroll ke bawah");
+        actions.swipeFromBottom(); 
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        
+        takeScreenshot("Leaderboard_Scrolled");
+        System.out.println("Selesai cek Leaderboard.");
+    }
+}
